f3522b63f8e8545c7e26f395ba417a26ef00dd64
shawn <chanceying@foxmail.com>
2026-09-16 23:06:35 +0800

    Resolve conflicts with master; add histogram operator

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -537,6 +537,7 @@ from flag_gems.ops.hash_tensor import hash_tensor
 from flag_gems.ops.heaviside import heaviside
 from flag_gems.ops.heaviside_ import heaviside_
 from flag_gems.ops.histc import histc
+from flag_gems.ops.histogram import histogram_bin_ct, histogram_bins_tensor
 from flag_gems.ops.histogramdd import histogramdd
 from flag_gems.ops.hsplit import hsplit
 from flag_gems.ops.hstack import hstack
@@ -1645,6 +1646,8 @@ __all__ = [
     "heaviside",
     "heaviside_",
     "histc",
+    "histogram_bin_ct",
+    "histogram_bins_tensor",
     "histogramdd",
     "hsplit",
     "hstack",
