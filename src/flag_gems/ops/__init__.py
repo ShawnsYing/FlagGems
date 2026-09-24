e23038f59ebe55ade4ab23257e8ed15ccb1ce9c6
ShawnsYing <chanceying@foxmail.com>
2026-09-24 16:13:01 +0800

    Add _linalg_det operator; rebuild registrations on latest master

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -112,6 +112,7 @@ from flag_gems.ops._is_all_true import _is_all_true
 from flag_gems.ops._jagged_to_padded_dense_forward import (
     _jagged_to_padded_dense_forward,
 )
+from flag_gems.ops._linalg_det import _linalg_det
 from flag_gems.ops._linalg_eigvals import _linalg_eigvals
 from flag_gems.ops._linalg_slogdet import _linalg_slogdet
 from flag_gems.ops._linalg_svd import _linalg_svd
@@ -1393,6 +1394,7 @@ __all__ = [
     "_index_put_impl_",
     "_is_all_true",
     "_jagged_to_padded_dense_forward",
+    "_linalg_det",
     "_linalg_eigvals",
     "_linalg_slogdet",
     "_linalg_svd",
